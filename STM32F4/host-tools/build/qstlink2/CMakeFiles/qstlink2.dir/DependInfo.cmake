18ab12912869215aac78ee9d2a7342823fea016c
Husam Abdulwahhab <snaketheslice@hotmail.com>
2013-04-22 16:55:41 +0200

    first commit

---
+++ STM32F4/host-tools/build/qstlink2/CMakeFiles/qstlink2.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/include/moc_devices.cxx" "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/CMakeFiles/qstlink2.dir/include/moc_devices.cxx.o"
+  "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/include/moc_dialog.cxx" "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/CMakeFiles/qstlink2.dir/include/moc_dialog.cxx.o"
+  "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/include/moc_mainwindow.cxx" "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/CMakeFiles/qstlink2.dir/include/moc_mainwindow.cxx.o"
+  "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/include/moc_stlinkv2.cxx" "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/CMakeFiles/qstlink2.dir/include/moc_stlinkv2.cxx.o"
+  "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/include/moc_transferthread.cxx" "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/CMakeFiles/qstlink2.dir/include/moc_transferthread.cxx.o"
+  "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/qrc_ressources.cxx" "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/CMakeFiles/qstlink2.dir/qrc_ressources.cxx.o"
+  "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/qstlink2/src/LibUsb.cpp" "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/CMakeFiles/qstlink2.dir/src/LibUsb.cpp.o"
+  "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/qstlink2/src/devices.cpp" "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/CMakeFiles/qstlink2.dir/src/devices.cpp.o"
+  "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/qstlink2/src/dialog.cpp" "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/CMakeFiles/qstlink2.dir/src/dialog.cpp.o"
+  "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/qstlink2/src/main.cpp" "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/CMakeFiles/qstlink2.dir/src/main.cpp.o"
+  "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/qstlink2/src/mainwindow.cpp" "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/CMakeFiles/qstlink2.dir/src/mainwindow.cpp.o"
+  "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/qstlink2/src/stlinkv2.cpp" "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/CMakeFiles/qstlink2.dir/src/stlinkv2.cpp.o"
+  "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/qstlink2/src/transferthread.cpp" "/home/snake/2013-mini-smart-vehicles/group-4/sources/STM32F4/host-tools/build/qstlink2/CMakeFiles/qstlink2.dir/src/transferthread.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "QT_GUI_LIB"
+  "QT_XML_LIB"
+  "QT_CORE_LIB"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )

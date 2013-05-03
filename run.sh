68da6c21d518493b9a57536df38f887cc8d58168
Sam <snaketheslice@hotmail.com>
2013-05-03 15:31:53 +0200

    updating the run file, the upload

--- run.sh
+++ run.sh
@@ -1,16 +1,3 @@
 #! /bin/bash
-
-RUN(){
-    gnome-terminal --execute bash -c "RUN_QST;bash"
-}
-
-
-
-RUN_QST (){
-    cd "/opt/msv/group-5/bin"
-    ./qstlink2
-}
-
-export -f RUN_QST
-
-RUN
+ cd /dev/
+ screen ttyACM0
